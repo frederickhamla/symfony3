69f75273d1cc7bcfa2216121d15660bd93e18108
frederickhamla <frederickkhamla@gmail.com>
2016-11-04 11:28:09 +0100

    [u][twbs] delete boostrap and assetic

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,6 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new AppBundle\AppBundle(),
-            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new ShagBundle\ShagBundle(),
         ];
 
